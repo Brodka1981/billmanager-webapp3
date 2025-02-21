45f8b2e6cb3b81d8db5b302c422ff19c43d839b0
Alessandro Mariani <brodka@gmail.com>
2025-02-21 17:36:06 +0100

    modifiche kubernetes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,13 @@
-export const environment = {
+/*export const environment = {
   production: true,
   apiAdmin: 'http://35.241.231.144/api/Admin',
   apiUrl: 'http://35.241.231.144/api/bills',
   apiPropertiesUrl: 'http://35.241.231.144/api/Properties'
+};*/
+
+export const environment = {
+  production: true,
+  apiAdmin: 'http://api-service.default.svc.cluster.local/api/Admin',
+  apiUrl: 'http://api-service.default.svc.cluster.local/api/bills',
+  apiPropertiesUrl: 'http://api-service.default.svc.cluster.local/api/Properties'
 };
