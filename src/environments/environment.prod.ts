56bc566de89e019137919ba532f509c2216877f9
Alessandro Mariani <brodka@gmail.com>
2025-03-03 13:00:54 +0100

    modifica punatmenti api

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,7 +7,7 @@
 
 export const environment = {
   production: true,
-  apiAdmin: 'http://35.205.45.171:30080/api/Admin',
-  apiUrl: 'http://35.205.45.171:30080/api/bills',
-  apiPropertiesUrl: 'http://35.205.45.171:30080/api/Properties'
+  apiAdmin: 'http://34.78.45.211:30080/api/Admin',
+  apiUrl: 'http://34.78.45.211:30080/api/bills',
+  apiPropertiesUrl: 'http://34.78.45.211:30080/api/Properties'
 };
