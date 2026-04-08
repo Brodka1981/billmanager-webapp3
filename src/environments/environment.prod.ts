695a522f0cd395201c15648c3ed075f0fbee57aa
Alessandro Mariani <brodka@gmail.com>
2026-04-08 17:45:41 +0200

    modifiche

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiAdmin: 'https://gestionebollette.duckdns.org/api/Admin',
-  apiUrl: 'https://gestionebollette.duckdns.org/api/bills',
-  apiPropertiesUrl: 'https://gestionebollette.duckdns.org/api/Properties'
+  apiAdmin: 'https://gestionebollette-web.duckdns.org:30443/api/Admin',
+  apiUrl: 'https://gestionebollette-web.duckdns.org:30443/api/bills',
+  apiPropertiesUrl: 'https://gestionebollette-web.duckdns.org:30443/api/Properties'
 };
